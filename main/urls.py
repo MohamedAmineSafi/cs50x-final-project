d1150fd0a3ed5c734c0709881294751cf60ee236
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-29 12:10:24 +0100

    Added 'Add Friend' Page

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "main"
 
-urlpatterns = [path("", views.HomePage.as_view(), name="index")]
+urlpatterns = [
+    path("", views.HomePage.as_view(), name="index"),
+    path("add/", views.Add.as_view(), name="add"),
+]
--- main/views.py
+++ main/views.py
@@ -16,3 +16,18 @@ class HomePage(TemplateView):
 
         context["name"] = "Amine"
         return context
+
+
+class Add(TemplateView):
+    http_method_names = ["get"]
+    template_name = "main/addFriend.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["name"] = "Amine"
+        return context
