1afdfa60a83e9e8e732ae527ca3457ffbe352203
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-29 18:17:29 +0100

    Implemented Login and Change Password

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
 from django.views import View
 from .models import User
 import hashlib
@@ -82,31 +81,109 @@ class RegisterPage(View):
         return response  # DONT FORGET
 
 
-class LoginPage(TemplateView):
-    http_method_names = ["get"]
-    template_name = "users/login.html"
+class LoginPage(View):
+    def get(self, req):
+        return render(req, "users/login.html")
 
-    def dispatch(self, request, *args, **kwargs):
-        self.request = request
-        return super().dispatch(request, *args, **kwargs)
+    def post(self, req):
+        # Get user data
+        res = parseBody(req.body)
+        username = res["username"]
+        password = res["password"]
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        try:
+            rememberMe = res["rememberMe"]
+        except:
+            rememberMe = "off"
 
-        context["name"] = "Amine"
-        return context
+        # Hash the password
+        salt = os.getenv("salt")
+        tmp = password + salt
+        hashedPassword = hashlib.sha256(tmp.encode("utf-8")).hexdigest()
+
+        # Get User
+        try:
+            userFromDB = User.objects.values().get(
+                username=username, password=hashedPassword
+            )
+            userFromDB = dict(userFromDB)
+        except User.DoesNotExist:
+            userFromDB = None
+
+        # Check Info
+        if userFromDB == None:
+            return throwError()
+
+        # Correct Name and Pass, Now update jwt and cookie
+
+        # Encode JWT
+        N = int(os.getenv("CookieLifeInMonths"))
+        expireDate = getNMonthsFromNow(N)
+        payload = {
+            "username": username,
+            "hashedPassword": hashedPassword,
+            "exp": expireDate,
+        }
+        SECRET_KEY = os.getenv("SECRET_KEY")
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+        # write new jwt to DB
+        user = User.objects.get(username=username, password=hashedPassword)
+        user.jwtToken = token
+        user.save()
+
+        # save cookie
+        response = HttpResponseRedirect("/")
+
+        if rememberMe == "on":
+            delta = timedelta(days=N * 30)
+            response.set_cookie(
+                "jwt_token", token, secure=False, httponly=False, max_age=delta
+            )
+        else:
+            response.set_cookie("jwt_token", token, secure=False, httponly=False)
+
+        return response
 
 
-class ChangePasswordPage(TemplateView):
-    http_method_names = ["get"]
-    template_name = "users/changePassword.html"
+class ChangePasswordPage(View):
+    def get(self, req):
+        return render(req, "users/changePassword.html")
 
-    def dispatch(self, request, *args, **kwargs):
-        self.request = request
-        return super().dispatch(request, *args, **kwargs)
+    def post(self, req):
+        # Get data from form
+        res = parseBody(req.body)
+        currentPass = res["currentPassword"]
+        newPassword = res["newPassword"]
+        cNewPassword = res["confirmPassword"]
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        # Make sure passwords are the same
+        if not (newPassword == cNewPassword):
+            return throwError()
 
-        context["name"] = "Amine"
-        return context
+        # Hash password
+        salt = os.getenv("salt")
+        tmp = currentPass + salt
+        hashedPassword = hashlib.sha256(tmp.encode("utf-8")).hexdigest()
+
+        # Get token from cookie
+        token = req.COOKIES["jwt_token"]
+        if not (bool(token)):
+            return throwError()
+
+        # Get user
+        try:
+            user = User.objects.get(password=hashedPassword, jwtToken=token)
+        except User.DoesNotExist:
+            return throwError()
+
+        # hash new pass
+        salt = os.getenv("salt")
+        tmp = newPassword + salt
+        hashNewPass = hashlib.sha256(tmp.encode("utf-8")).hexdigest()
+
+        # save user
+        user.password = hashNewPass
+        user.jwtToken = ""
+        user.save()
+        return redirect("/")
