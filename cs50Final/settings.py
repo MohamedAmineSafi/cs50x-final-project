8264f7f5a792f1eae42dbb83128fb287351cbec6
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-05-29 20:24:51 +0100

    I am an idiot!

--- cs50Final/settings.py
+++ cs50Final/settings.py
@@ -18,11 +18,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_path = Path(os.path.join(PROJECT_DIR, "config.env"))
+load_dotenv(dotenv_path=dotenv_path)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zt=5+m&-$@+hk86lh7sj)je*%#w9gr_hp@ohp2f2l^8pi_5cy_'
+# SECRET_KEY = 'django-insecure-zt=5+m&-$@+hk86lh7sj)je*%#w9gr_hp@ohp2f2l^8pi_5cy_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +89,7 @@ DATABASES = {
         'NAME': 'Cluster0',
         'ENFORCE_SCHEMA': True,
         'CLIENT': {
-            'host': 'mongodb+srv://mohammedameenalsafi:C9LY1w4W7ny8Dp63@cluster0.m4bgnbj.mongodb.net/?retryWrites=true&w=majority'
+            'host': os.getenv("DBUrl"),
         }  
     }
 }
@@ -138,6 +141,5 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-dotenv_path = Path(os.path.join(PROJECT_DIR, "config.env"))
-load_dotenv(dotenv_path=dotenv_path)
+
 SECRET_KEY = os.getenv("SECRET_KEY")
